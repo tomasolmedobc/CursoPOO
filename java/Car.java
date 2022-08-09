c680edd4fcdf674ec2fcb8a18c7b21211ca47f0e
Tomas Olmedo <tomasolmedobc@gmail.com>
2022-08-09 11:58:47 -0300

    Declarando un Método constructor en Java

--- java/Car.java
+++ java/Car.java
@@ -1,11 +1,16 @@
 public class Car {
     Integer id;
     String  license;
-    String driver;
+    Account driver;
     Integer passegenger;
+
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
 
     //Declarando un metodo para traer los datos de la Clase Car
     void printDataCar(){
-        System.out.println("License: " + license + " Driver: " + driver);
+        System.out.println("License: " + license + " Name Driver: " + driver.name);
     }
-    }
+}
--- java/Main.java
+++ java/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+        //car.license = "AMQ123";
+        //car.driver = "Andres Herrera";
         car.passegenger = 4;
         car.printDataCar();
         //System.out.println("Car license: " + car.license);
         
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver ="Andrea Herrera";
+        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+       // car2.license = "QWE567";
+        //car2.driver ="Andrea Herrera";
         car2.passegenger = 3;
        // System.out.println("Car License: " + car2.license);
         car2.printDataCar();
